9c69984112b94c46ae4357c9ff17126bd0fc7d9f
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2026-04-07 18:59:38 +0100

    Resolve merge conflicts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
-import { configDefaults } from 'vitest/config'
-=======
->>>>>>> 7c10519 (upload coding challenge)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { configDefaults } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-<<<<<<< HEAD
   test: {
-    environment: 'happy-dom',
-    setupFiles: './src/setupTests.js',
+    environment: "happy-dom",
+    setupFiles: "./src/setupTests.js",
     globals: true,
     css: false,
-    exclude: [...configDefaults.exclude, 'node_modules'],
+    exclude: [...configDefaults.exclude, "node_modules"],
   },
-=======
->>>>>>> 7c10519 (upload coding challenge)
-})
+});
